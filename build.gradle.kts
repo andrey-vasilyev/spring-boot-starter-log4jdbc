d62529b67b870b2e4c3909ade8b6d4cec574d11c
Andrey <andrey-vasilyev@users.noreply.github.com>
2023-10-14 00:04:53 +0300

    setting release

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,15 @@ plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.deps.management)
     pmd
+
+    `maven-publish`
+    signing
+    alias(libs.plugins.axion.release)
+    alias(libs.plugins.nexus.publish)
 }
 
 group = "ru.vasiand"
+version = scmVersion.version
 
 repositories {
     mavenCentral()
@@ -63,4 +69,66 @@ tasks.test {
 pmd {
     toolVersion = "6.23.0"
     ruleSetFiles = files("pmd.xml")
+}
+
+scmVersion {
+    localOnly.set(true)
+    with(tag) {
+        prefix.set("")
+        versionSeparator.set("")
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
+
+tasks.withType<Jar> {
+    from(rootProject.projectDir) {
+        include("LICENSE.txt")
+        into("META-INF")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("The Log4jdbc Spring Boot 3 Starter facilitates the quick and easy use of http://log4jdbc.brunorozendo.com/ in Spring Boot projects.")
+                url.set("https://github.com/andrey-vasilyev/spring-boot-starter-log4jdbc")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("andrey-vasilyev")
+                        name.set("Andrey Vasilyev")
+                        email.set("vasilyev.andrew@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/andrey-vasilyev/spring-boot-starter-log4jdbc")
+                }
+            }
+        }
+    }
+}
+
+val gpgKey: String? = System.getenv("GPG_KEY")
+val gpgPass: String? = System.getenv("GPG_PASS")
+signing {
+    isRequired = gpgKey != null
+    useInMemoryPgpKeys(gpgKey, gpgPass)
+    sign(publishing.publications["mavenJava"])
 }
--- src/main/java/ru/vasiand/spring/boot/log4jdbc/Log4jdbcAutoConfiguration.java
+++ src/main/java/ru/vasiand/spring/boot/log4jdbc/Log4jdbcAutoConfiguration.java
@@ -1,11 +1,10 @@
 package ru.vasiand.spring.boot.log4jdbc;
 
 import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
-
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -13,9 +12,9 @@ import org.springframework.context.annotation.Bean;
  * {@link EnableAutoConfiguration Auto-configuration} for log4jdbc.
  *
  */
-@AutoConfiguration
+@AutoConfiguration(after = DataSourceAutoConfiguration.class)
 @ConditionalOnClass(DataSourceSpy.class)
-@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+@ConditionalOnProperty(name = "log4jdbc.spy.enabled", havingValue = "true", matchIfMissing = true)
 public class Log4jdbcAutoConfiguration {
 	@Bean
 	public static Log4jdbcBeanPostProcessor log4jdbcBeanPostProcessor() {
